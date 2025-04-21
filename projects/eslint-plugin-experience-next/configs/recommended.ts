c4b14520bdd9b0ef308f1eded1c834f2fb311df2
splincode <omaxphp@yandex.ru>
2025-04-21 10:01:19 +0300

    feat(eslint-plugin-experience-next): do not use `Attribute` decorator

--- projects/eslint-plugin-experience-next/configs/recommended.ts
+++ projects/eslint-plugin-experience-next/configs/recommended.ts
@@ -704,6 +704,11 @@ export default tseslint.config(
                 },
                 {
                     message:
+                        'Always prefer using HostAttributeToken over @Attribute. See: https://angular.dev/api/core/HostAttributeToken',
+                    selector: "Identifier[name='Attribute']",
+                },
+                {
+                    message:
                         'Always prefer using the host property over @HostBinding. That decorator exists exclusively for backwards compatibility',
                     selector: "Identifier[name='HostBinding']",
                 },
