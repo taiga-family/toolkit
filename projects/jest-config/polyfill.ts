28482a0b7394c76337566b4af16590528af710cd
splincode <omaxphp@yandex.ru>
2025-08-05 12:01:10 +0300

    chore: update

--- projects/jest-config/polyfill.ts
+++ projects/jest-config/polyfill.ts
@@ -1,12 +1,10 @@
-import type {TimerOptions} from 'node:timers';
-
 if (typeof globalThis.structuredClone !== 'function') {
     globalThis.structuredClone = (obj: unknown) => JSON.parse(JSON.stringify(obj));
 }
 
 if (typeof globalThis.setImmediate !== 'function') {
-    globalThis.setImmediate = (<T>(value?: T, options?: TimerOptions) =>
-        setTimeout(() => value, 0, options)) as unknown as typeof globalThis.setImmediate;
+    globalThis.setImmediate = ((fn: (...args: unknown[]) => void, ...args: unknown[]) =>
+        setTimeout(fn, 0, ...args)) as unknown as typeof globalThis.setImmediate;
 }
 
 if (typeof globalThis.clearImmediate !== 'function') {
