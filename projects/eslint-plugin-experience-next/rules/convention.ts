73c5a80ccca534e4c3ad4056e28db1cf09c3ae21
Максим Иванов <omaxphp@yandex.ru>
2025-09-01 14:26:13 +0300

    fix(eslint-plugin-experience-next): revert restrict non-exported naming (#1243)

--- projects/eslint-plugin-experience-next/rules/convention.ts
+++ projects/eslint-plugin-experience-next/rules/convention.ts
@@ -1,145 +1,35 @@
 export const TUI_RECOMMENDED_NAMING_CONVENTION = [
     {
-        format: ['PascalCase', 'UPPER_CASE'],
-        selector: 'typeLike',
-    },
-    {
         format: ['PascalCase'],
-        selector: 'class',
-    },
-    {
-        format: ['PascalCase'],
-        modifiers: ['abstract'],
-        selector: 'class',
+        selector: ['class', 'interface', 'typeAlias', 'typeLike', 'enum', 'enumMember'],
     },
     {
         format: ['camelCase'],
-        selector: 'function',
+        selector: ['classMethod', 'function', 'classProperty'],
     },
     {
-        format: ['PascalCase'],
-        selector: 'interface',
-    },
-    {
-        format: ['PascalCase'],
-        selector: 'typeAlias',
-    },
-    {
-        format: null,
-        modifiers: ['destructured'],
-        selector: 'variable',
+        format: ['UPPER_CASE', 'camelCase', 'PascalCase'],
+        selector: ['variable'],
     },
     {
         filter: '__non_webpack_require__',
         format: null,
         selector: 'variable',
     },
-    {
-        format: ['camelCase', 'UPPER_CASE'],
-        selector: 'variable',
-    },
-    {
-        format: ['UPPER_CASE', 'camelCase', 'PascalCase'],
-        modifiers: ['global'],
-        selector: 'variable',
-    },
-    {
-        format: ['UPPER_CASE', 'camelCase', 'PascalCase'],
-        selector: 'variable',
-    },
-    {
-        format: ['PascalCase'],
-        modifiers: ['abstract'],
-        selector: 'class',
-    },
-    {
-        format: ['StrictPascalCase'],
-        selector: 'enum',
-    },
-    {
-        format: ['PascalCase'],
-        selector: 'enumMember',
-    },
-    {
-        format: ['camelCase'],
-        selector: 'classMethod',
-    },
-    {
-        format: ['camelCase', 'UPPER_CASE'],
-        selector: 'classProperty',
-    },
 ] as const;
 
 export const TUI_CUSTOM_TAIGA_NAMING_CONVENTION = [
+    ...TUI_RECOMMENDED_NAMING_CONVENTION,
     {
-        format: ['PascalCase', 'UPPER_CASE'],
-        selector: 'typeLike',
+        format: ['PascalCase'],
+        modifiers: ['exported'],
+        prefix: ['Tui', 'AbstractTui'],
+        selector: ['class', 'interface', 'typeAlias', 'typeLike', 'enum'],
     },
     {
         format: ['PascalCase'],
-        prefix: ['Tui'],
-        selector: 'class',
-    },
-    {
-        format: ['PascalCase'],
-        modifiers: ['abstract'],
-        prefix: ['AbstractTui', 'Tui'],
-        selector: 'class',
-    },
-    {
-        format: ['PascalCase'],
+        modifiers: ['exported'],
         prefix: ['tui'],
         selector: 'function',
-    },
-    {
-        format: ['PascalCase'],
-        prefix: ['Tui'],
-        selector: 'interface',
-    },
-    {
-        format: ['PascalCase'],
-        prefix: ['Tui'],
-        selector: 'typeAlias',
-    },
-    {
-        format: null,
-        modifiers: ['destructured'],
-        selector: 'variable',
-    },
-    {
-        format: ['camelCase'],
-        selector: 'variable',
-    },
-    {
-        format: ['UPPER_CASE', 'camelCase', 'PascalCase'],
-        modifiers: ['global'],
-        selector: 'variable',
-    },
-    {
-        format: ['UPPER_CASE', 'camelCase', 'PascalCase'],
-        selector: 'variable',
-    },
-    {
-        format: ['PascalCase'],
-        modifiers: ['abstract'],
-        prefix: ['AbstractTui', 'Tui'],
-        selector: 'class',
-    },
-    {
-        format: ['StrictPascalCase'],
-        prefix: ['Tui'],
-        selector: 'enum',
-    },
-    {
-        format: ['PascalCase'],
-        selector: 'enumMember',
-    },
-    {
-        format: ['camelCase'],
-        selector: 'classMethod',
-    },
-    {
-        format: ['camelCase'],
-        selector: 'classProperty',
     },
 ] as const;
