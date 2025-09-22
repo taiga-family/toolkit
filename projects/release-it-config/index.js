b5ebea3eb3fa1ef286a9c1d39215af6d1acdd40a
Максим Иванов <omaxphp@yandex.ru>
2025-09-22 10:26:26 +0300

    fix(release-it-config): there are no commits since the latest tag. (#1279)

--- projects/release-it-config/index.js
+++ projects/release-it-config/index.js
@@ -50,7 +50,7 @@ module.exports = {
         ],
         'after:release':
             'echo Successfully released ${name} v${version} to ${repo.repository}.',
-        'before:init': 'git fetch --prune --prune-tags origin',
+        'before:init': 'git fetch --prune --prune-tags origin > /dev/null || echo ""',
         'before:release': 'npm run release || echo ""',
     },
     npm: {
