d861ea6df2e8def468be0f5f5eb46d98f576ed04
Максим Иванов <omaxphp@yandex.ru>
2025-10-16 19:33:58 +0300

    feat(eslint-plugin-experience-next): forbid import bypassing entrypoint (#1334)

--- projects/eslint-plugin-experience-next/index.ts
+++ projects/eslint-plugin-experience-next/index.ts
@@ -1,5 +1,6 @@
+import {readFileSync} from 'node:fs';
+
 import {type Linter} from 'eslint';
-import {readFileSync} from 'fs';
 import {type ConfigArray} from 'typescript-eslint';
 
 import htmlEslint from './configs/html-eslint';
@@ -10,6 +11,7 @@ import classPropertyNaming from './rules/class-property-naming';
 import decoratorKeySort from './rules/decorator-key-sort';
 import injectionTokenDescription from './rules/injection-token-description';
 import noDeepImports from './rules/no-deep-imports';
+import noDeepImportsToIndexedPackages from './rules/no-deep-imports-to-indexed-packages';
 import noHrefWithRouterLink from './rules/no-href-with-router-link';
 import noImplicitPublic from './rules/no-implicit-public';
 import noPrivateEsnextFields from './rules/no-private-esnext-fields';
@@ -35,6 +37,7 @@ const plugin = {
         'decorator-key-sort': decoratorKeySort,
         'injection-token-description': injectionTokenDescription,
         'no-deep-imports': noDeepImports,
+        'no-deep-imports-to-indexed-packages': noDeepImportsToIndexedPackages,
         'no-href-with-router-link': noHrefWithRouterLink,
         'no-implicit-public': noImplicitPublic,
         'no-private-esnext-fields': noPrivateEsnextFields,
