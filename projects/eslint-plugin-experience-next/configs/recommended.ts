674e40f6b6086ce7bf1af34a7777f70695e02aff
splincode <omaxphp@yandex.ru>
2025-10-23 14:16:13 +0300

    feat(eslint-plugin-experience-next): ignore button type within `tuiButtonClose`

--- projects/eslint-plugin-experience-next/configs/recommended.ts
+++ projects/eslint-plugin-experience-next/configs/recommended.ts
@@ -785,6 +785,7 @@ export default tseslint.config(
                 'error',
                 {
                     ignoreWithDirectives: [
+                        'tuiButtonClose',
                         'tuiAccordion',
                         'tuiOption',
                         'tuiStep',
