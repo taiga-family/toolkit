515dc343efd51282fe342fc7c267e1da4207d19f
Максим Иванов <omaxphp@yandex.ru>
2026-03-20 22:52:39 +0900

    feat(stylelint-config): bump stylelint-plugin-use-baseline (#1658)

--- projects/stylelint-config/index.js
+++ projects/stylelint-config/index.js
@@ -27,6 +27,10 @@ module.exports = {
         `${process.cwd()}/**/node_modules/**`,
         `${process.cwd()}/**/tests-report/**`,
     ],
+    reportDescriptionlessDisables: true,
+    reportNeedlessDisables: true,
+    reportUnscopedDisables: true,
+    validate: true,
     rules: {
         '@stylistic/declaration-block-trailing-semicolon': null,
         '@stylistic/declaration-colon-newline-after': null,
@@ -302,6 +306,7 @@ module.exports = {
                     'focus-visible', // Safari 15+
                     'fullscreen', // Safari 16+
                 ],
+                ignoreUnits: ['svh'],
             },
         ],
         'property-disallowed-list': [
