9c4af8da42ee69447bf568511702bb219c861f30
Максим Иванов <omaxphp@yandex.ru>
2026-04-10 16:26:56 +0900

    fix(eslint-plugin-experience-next): valid configuration (#1723)

--- projects/eslint-plugin-experience-next/index.ts
+++ projects/eslint-plugin-experience-next/index.ts
@@ -70,6 +70,7 @@ Object.assign(plugin.configs, {
     ],
     recommended: [
         {files: ALL_TS_JS_FILES, plugins: {'@taiga-ui/experience-next': plugin}},
+        {files: ['**/*.html'], plugins: {'@taiga-ui/experience-next': plugin}},
         ...recommended,
     ],
     ['taiga-specific']: [
